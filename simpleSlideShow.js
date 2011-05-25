7b5507f666265bbc7f5c44d258d352eae3e573af
nthomson <nthomsondev@gmail.com>
2011-05-25 17:33:22 -0400

    Add thumbnail support for cirrus

--- simpleSlideShow.js
+++ simpleSlideShow.js
@@ -1,4 +1,4 @@
-function simpleSlideShow(element, interval, next_element, prev_element, pause_element, play_element) {
+function simpleSlideShow(element, interval, next_element, prev_element, thumbnails) {
 
     var slideshow_next = function(el) {
         var $el = $(el);
@@ -17,7 +17,7 @@ function simpleSlideShow(element, interval, next_element, prev_element, pause_el
                 $el.data('locked', 'False');
             });
         }
-    };
+    }
     
     var slideshow_prev = function(el){
         var $el = $(el);
@@ -38,16 +38,44 @@ function simpleSlideShow(element, interval, next_element, prev_element, pause_el
         }
     };
     
-    var slideshow_pause = function() {
-    	clearInterval(interval_id);
-    };
-    
-    var slideshow_play = function() {
-    	interval_id = setInterval(function(){slideshow_next(element)}, interval);
-    };
+    var slideshow_goto = function(el, next) {
+      //Stop the auto-animation
+      if(interval !== false){clearInterval(mInt);}
+      
+      var $el = $(el);
+      if ($el.data('locked') == 'False') {
+      
+        //Get all the elements we need
+        $el.data('locked', 'True');
+        var $el = $(el);
+        var $next = $el.children('.slideitem-'+next);
+        var $current = $el.children(':first');
+        
+        //Fade out current element, then fade in new element, get positioning right.
+        $current.fadeOut(500, 
+        function(){
+          $current.css('left', ss_width);
 
-	var interval_id;
-    var $el = $(element);
+          //prevAll() returns in the reverse order of what we need, so we have to reverse it before we append
+          $el.append( Array.prototype.reverse.call( $next.prevAll() ) );
+          
+          $next.hide();
+          $next.css('left', '0%');
+         
+          $next.fadeIn(500, function(){
+            $current.show();
+            $el.data('locked', 'False');
+          });
+          
+          //Restart the auto-animation
+          if(interval !== false){mInt = setInterval(function(){slideshow_next(element)}, interval);}
+        });
+        
+        //Append all elements before the next element to end   
+      } 
+    }
+
+    var $el = $(element)
     var ss_width = '100%';
     var ss_height = $el.height();
     $el.css({
@@ -60,27 +88,61 @@ function simpleSlideShow(element, interval, next_element, prev_element, pause_el
         'height': ss_height+'px',
         'left': ss_width
     });
+    $el.children().each(function(index){
+      $(this).attr('class', 'slideitem-'+index); 
+    });
     $el.children(':first').css({
         'left': '0%'
     });
     $el.data('locked', 'False');
 
     if (next_element !== undefined) {
-        $(next_element).click(function(){slideshow_next(element)});
+        $(next_element).click(
+          function(){
+            if(tActive < $(element).children().size() - 1){
+              tActive++;
+            }
+            else{
+              tActive = 0;
+            }
+            moveThumb();
+            slideshow_next(element);
+        });
     }
     if (prev_element !== undefined) {
-        $(prev_element).click(function(){slideshow_prev(element)});
+        $(prev_element).click(function(){
+          if(tActive > 0) {
+            tActive--;
+          }
+          else{
+            tActive = $(element).children().size() - 1;
+          }
+          moveThumb();
+          slideshow_prev(element);
+        });
     }
     if (interval === undefined) {
         interval = 5000;
     }
+    if (thumbnails !== undefined) {
+      $(thumbnails).children().each(function(index) {
+          var thumbNum = index;
+          $(this).click(function(){
+              slideshow_goto(element, thumbNum);
+              tActive = thumbNum;
+              moveThumb();
+          });
+      });
+    }
+    
+    var moveThumb = function() {
+      $(thumbnails).children().removeClass('selected');
+      $(thumbnails).children().eq(tActive).addClass('selected');
+      $('#thumb-selected').animate( { left: tActive*79 }, 600 );
+
+    }
+    
     if (interval !== false) {
-        slideshow_play();
-    }
-    if (pause_element !== undefined) {
-    	$(pause_element).click(function(){slideshow_pause()});
-    }
-    if (play_element !== undefined) {
-    	$(play_element).click(function(){slideshow_play()});
+        mInt = setInterval(function(){slideshow_next(element)}, interval);
     }
 }
